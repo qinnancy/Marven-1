a8d6a23c2c4946cfa5d240351056a2c2f8aebb80
sjiang@deloitte.com.cn <sjiang@deloitte.com.cn>
2022-01-23 20:08:14 +0800

    update get news image method

--- code/backend/REAccess.Mobile.Common/Services/RankService.cs
+++ code/backend/REAccess.Mobile.Common/Services/RankService.cs
@@ -147,8 +147,8 @@ namespace REAccess.Mobile.Common.Services
                 Id = x.Id,
                 NewsTitle = x.Title,
                 NewsContent = x.Content,
-                //NewsImage = Path.Combine("RealTimeInfoImgs",x.Img),
-                NewsImage = FileEncode.GetImageBytes($"{Directory.GetCurrentDirectory()}\\RealTimeInfoImgs\\{x.Img}"),
+                NewsImage = Path.Combine("RealTimeInfoImgs",x.Img),
+                //NewsImage = FileEncode.GetImageBytes($"{Directory.GetCurrentDirectory()}\\RealTimeInfoImgs\\{x.Img}"),
                 NewsReleaseDate = Convert.ToDateTime(x.ReleaseTime, dtFormat).ToString("yyyy-MM-dd"),
                 NewTags = x.Tags.Split(';').ToList()
             }).ToList();
@@ -169,7 +169,7 @@ namespace REAccess.Mobile.Common.Services
                 model.Id = dbNews.Id;
                 model.NewsTitle = dbNews.Title;
                 model.NewsContent = dbNews.Content;
-                model.NewsImage = FileEncode.GetImageBytes($"{Directory.GetCurrentDirectory()}\\RealTimeInfoImgs\\{dbNews.Img}");
+                model.NewsImage = Path.Combine("RealTimeInfoImgs", dbNews.Img);
                 model.NewsReleaseDate = Convert.ToDateTime(dbNews.ReleaseTime, dtFormat).ToString("yyyy-MM-dd");
                 model.NewTags = dbNews.Tags.Split(';').ToList();
             }
